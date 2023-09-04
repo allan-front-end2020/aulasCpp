5b6ef2f2185d4ed3202388e86313ae788ecef230
Allan Tavares <allan.tavares@estudante.ufla.br>
2023-09-04 08:54:09 -0300

    first commit

---
+++ aula01/primeiro.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int idade;
+
+    // float dinheiro;
+    // dinheiro = 5.70;
+    // cout << dinheiro << endl;
+
+    // double vida;
+    // vida = 5.3e99;
+    // cout << vida << endl;
+
+    // bool boleano;
+    // boleano = true;
+    // cout << boleano << endl;
+
+    // char letrar;
+    // letrar = 'M';
+    // cout << letrar << endl;
+
+    // string palavras;
+    // palavras = "Allan e foda ";
+    // cout << palavras << endl;
+
+    cout << "qual e sua idade" << endl;
+    cin >> idade;
+    cout <<"sua idade e" << idade << endl;
+
+    return 0;
+}
